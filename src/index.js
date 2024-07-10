94e5752d8b0af7af5700804864de490d72de25b2
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-10 11:27:24 +0700

    Change localStorage/localSave function into IIFE

--- src/index.js
+++ src/index.js
@@ -25,37 +25,40 @@ const projectSelector = (() => {
     };
 
     return (set, get);
-})
+});
 
 const createProject = () => {
     const project = [];
     return project;
-}
+};
 
-const localStorage = (mode, key, item) => {
-    if (mode === "get") {
-        const get = localStorage.getItem(key);
-        return JSON.parse(get);
-    } else if (mode === "set") {
+const localSave = (() => {
+    const get = (key) => {
+        const item = localStorage.getItem(key);
+        return JSON.parse(item);
+    }
+    const set = (key, item) => {
         localStorage.setItem(key, JSON.stringify(item));
     }
-}
+
+    return {get, set};
+})();
 
 const checkLocalProjectList = () => {
-    if (!localStorage.getItem("projects")) {
+    if (!localSave.get("projects")) {
         // Creates new default project and saves it locally
         projects.addToList(createProject());
-        localStorage("set", "projects", projectList.get());
+        localSave.set("projects", projectList.get());
         projectSelector.get(0);
-        localStorage('set', 'selector', 0);
+        localSave.set('selector', 0);
     } else {
-        const localList = localStorage('get', 'projects');
+        const localList = localSave.get("projects");
         for (const project of localList) {
             projectList.add(project);
         };
     };
-}
+};
 
 const todo = (title, description, dueDate, priority, notes, checklist) => {
     return {title, description, dueDate, priority, notes, checklist};
-}
+};
