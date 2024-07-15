269b6758fe33f0ffc86f43e3633897a6afb36250
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-15 16:15:06 +0700

    Create a way to create inputs when making todos

--- src/dom.js
+++ src/dom.js
@@ -32,6 +32,76 @@ const appendPreviousSelectedProject = (list, selectedProject) => {
     };
 };
 
+
+
+const todoInputs = {
+    // Creates the placeholder text for the inputs
+    placeholderText: function(text, input) {
+        input.setAttribute('placeholder', text);
+    },
+    // Creates the inputs
+    content: function(name, type) {
+        let label;
+        if (type !== 'submit') {
+            label = document.createElement('label');
+            label.innerHTML = name;
+            label.setAttribute('for', name);
+        };
+        
+        let input;
+        if (type === 'textarea') {
+            input = document.createElement('textarea');
+            input.setAttribute('rows', "10");
+            input.setAttribute('columns', "50");
+        } else {
+            input = document.createElement('input');
+            input.setAttribute('type', type);
+        };
+        
+        if (type === 'submit') {
+            input.setAttribute('value', name);
+        } else if (type !== 'textarea') {
+            input.setAttribute('id', name);
+            input.setAttribute('name', name);
+            input.setAttribute('required', "");
+        };
+
+        let datalist;
+        if (type === 'range') {
+            input.setAttribute('list', 'priority');
+            datalist = document.createElement('datalist');
+            datalist.setAttribute('id', 'priority');
+            
+            const low = document.createElement('option');
+            low.setAttribute('value', '0');
+            low.innerHTML = 'Low';
+            datalist.appendChild(low);
+
+            const mid = document.createElement('option');
+            mid.setAttribute('value', '1');
+            mid.innerHTML = 'Mid';
+            datalist.appendChild(mid);
+
+            const high = document.createElement('option');
+            high.setAttribute('value', '2');
+            high.innerHTML = 'High';
+            datalist.appendChild(high);
+        };
+
+        if (type === "text" && name === "Title") this.placeholderText("E.g. Taking the bin out, do my homeworks, etc", input);
+        if (type === "textarea") this.placeholderText("Details of your to-dos (optional) ", input);
+        if (type === "text" && name === "Notes") this.placeholderText("Add any additional information on here...", input);
+
+        const section = document.createElement('div');
+        if (type !== 'submit') section.appendChild(label);
+        section.appendChild(input);
+        if (type === 'range') section.appendChild(datalist);
+        section.className = name.toLowerCase().split(" ").join("");
+        
+        return section;
+    }
+};
+
 const appendTodo = (project, todo) => {
     const todoCard = document.createElement('li');
 
